9b8d36d62ae0540d334786dba6687d27a567b4c1
Nitin <60922929+NitinShelke@users.noreply.github.com>
2022-02-15 19:46:16 +0530

    Update app.py

--- ImageClassification_soccer_V_rugby/app.py
+++ ImageClassification_soccer_V_rugby/app.py
@@ -1,15 +1,17 @@
-
 import os
 import numpy as np
 
-from keras.models import load_model
-from keras.preprocessing.image import img_to_array
+from keras.models import  load_model
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import cv2
 
+UPLOAD_FOLDER = 'Uploads'
+
 # Define a flask app
 app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def model_predict(img_path):
     img=cv2.imread(img_path)
@@ -17,13 +19,16 @@ def model_predict(img_path):
     img=cv2.resize(img,(224,224))
     img=np.array(img)
     img=img.reshape((-1,img.shape[0],img.shape[1],img.shape[2]))
+    basepath = os.path.dirname(__file__)
+    model_path = os.path.join(
+        basepath, 'model.h5')
 
-    mymodel = load_model('model.h5')
-    y = mymodel.predict(a)
+    mymodel = load_model(model_path)
+    y = mymodel.predict(img)
     if (int(y))==0:
-        return "Rugby"
+        return ("Rugby")
     else:
-        return "Soccer"
+        return ("Soccer")
 
     return pred
 
@@ -39,16 +44,16 @@ def upload():
         # Get the file from post request
         f = request.files['file']
 
-        # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
+            basepath, 'Uploads', secure_filename(f.filename))
         f.save(file_path)
 
         result = model_predict(file_path)
-        return result
+        return (result)
     return None
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
